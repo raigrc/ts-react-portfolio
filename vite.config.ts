6f61fd673a5e4ecb03b7cf512aad790791241ffc
raigrc <ravenjstn.grc@gmail.com>
2024-05-22 11:45:21 +0800

    fix base route

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/ts-react-portfolio/",
+  base: "/ts-react-portfolio",
   plugins: [react()],
   resolve: {
     alias: {
