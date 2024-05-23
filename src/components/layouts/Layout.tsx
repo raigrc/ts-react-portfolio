e23dd7cb686af2ffb63db035920a5a3f2ec19234
raigrc <ravenjstn.grc@gmail.com>
2024-05-23 15:15:27 +0800

    responsive navigation bar

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import {
   NavigationMenu,
+  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
+  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import {
@@ -17,6 +19,7 @@ import { useTheme } from "@/components/theme-provider";
 import { Button } from "../ui/button";
 
 import { Sun, Moon } from "lucide-react";
+import { FaBars } from "react-icons/fa6";
 
 const Layout: React.FC = () => {
   const { setTheme } = useTheme();
@@ -32,7 +35,7 @@ const Layout: React.FC = () => {
             </Link>
           </NavigationMenuItem>
 
-          <NavigationMenuItem>
+          <NavigationMenuItem className="hidden md:block">
             <NavLink
               to="/"
               className={({ isActive }) =>
@@ -89,7 +92,7 @@ const Layout: React.FC = () => {
             </NavLink>
           </NavigationMenuItem>
 
-          <NavigationMenuItem>
+          <NavigationMenuItem className="hidden md:block">
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button variant="outline">
@@ -108,6 +111,113 @@ const Layout: React.FC = () => {
                 <DropdownMenuItem onClick={() => setTheme("system")}>
                   System
                 </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem className="space-x-2 md:hidden">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline">
+                  <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                  <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                  <span className="sr-only">Toggle theme</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                  Light
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                  Dark
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("system")}>
+                  System
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button
+                  variant="outline"
+                  className="transition-all duration-200"
+                >
+                  <FaBars className="duration-200 size-5 hover:fill-primary" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive ? "font-bold text-primary" : ""
+                  }
+                >
+                  <DropdownMenuItem>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      Home
+                    </NavigationMenuLink>
+                  </DropdownMenuItem>
+                </NavLink>
+
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) =>
+                    isActive ? "font-bold  text-primary" : ""
+                  }
+                >
+                  <DropdownMenuItem>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      About
+                    </NavigationMenuLink>
+                  </DropdownMenuItem>
+                </NavLink>
+
+                <NavLink
+                  to="/projects"
+                  className={({ isActive }) =>
+                    isActive ? "font-bold  text-primary" : ""
+                  }
+                >
+                  <DropdownMenuItem>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      Projects
+                    </NavigationMenuLink>
+                  </DropdownMenuItem>
+                </NavLink>
+                <NavLink
+                  to="/skills"
+                  className={({ isActive }) =>
+                    isActive ? "font-bold  text-primary" : ""
+                  }
+                >
+                  <DropdownMenuItem>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      Skills
+                    </NavigationMenuLink>
+                  </DropdownMenuItem>
+                </NavLink>
+                <NavLink
+                  to="/contacts"
+                  className={({ isActive }) =>
+                    isActive ? "font-bold  text-primary" : ""
+                  }
+                >
+                  <DropdownMenuItem>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      Contacts
+                    </NavigationMenuLink>
+                  </DropdownMenuItem>
+                </NavLink>
               </DropdownMenuContent>
             </DropdownMenu>
           </NavigationMenuItem>
