6c3c71305fb6c9326929a9f703c327305350d3dd
raigrc <ravenjstn.grc@gmail.com>
2024-05-27 19:34:57 +0800

    add dialog component

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -14,53 +14,40 @@ import Calc2 from "/projects/calc-2.jpg";
 import HTMLLogo from "/skills/file-type-html.svg";
 import CSSLogo from "/skills/file-type-css.svg";
 import JSLogo from "/skills/file-type-js-official.svg";
+import PHPLogo from "/skills/file-type-php.svg";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const Projects: React.FC = () => {
   useDocumentTitle("Projects");
 
   const allProjects = [
     {
-      name: "Basic Calculator",
+      name: "E-Commerce Website",
       techStack: {
         html: HTMLLogo,
         css: CSSLogo,
         js: JSLogo,
+        php: PHPLogo,
       },
       firstBg: Calc1,
       secondBg: Calc2,
+      dateCreated: "February 28, 2023",
     },
   ];
   return (
     <section className="h-full px-3">
       <div className="max-w-screen-xl py-3 mx-auto">
         <div className="flex h-full space-x-3">
-          {/* <Card className="relative flex px-0 transition-all duration-500 group size-56 hover:flex-1">
-            <div className="absolute w-full h-full transition-all duration-300">
-              <img
-                className="w-full h-full transition-all duration-500 delay-300 bg-red-500 opacity-30 grayscale group-hover:grayscale-0"
-                src=""
-                alt=""
-              />
-            </div>
-
-            <div className="relative h-full">
-              <CardHeader className="h-1/4">
-                <CardTitle className="">Simple Calculator</CardTitle>
-              </CardHeader>
-              <CardContent className="absolute transition-all opacity-0 h-2/4 duration-0 group-hover:relative group-hover:opacity-100 group-hover:duration-1000 ">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Beatae quis omnis autem eius veniam ratione.
-                </p>
-              </CardContent>
-              <CardFooter className="absolute justify-end opacity-0 h-1/4 group-hover:relative group-hover:flex group-hover:opacity-100 group-hover:transition-all group-hover:delay-300 group-hover:duration-1000">
-                <Button>Read more</Button>
-              </CardFooter>
-            </div>
-          </Card> */}
           {allProjects.map((project, index) => (
             <Card
-              className="relative w-full h-56 px-0 overflow-hidden transition-all duration-500 group md:w-64 md:hover:w-96"
+              className="relative w-full h-48 px-0 overflow-hidden transition-all duration-500 group md:w-64 md:hover:w-96"
               key={index}
             >
               <img
@@ -76,7 +63,7 @@ const Projects: React.FC = () => {
                 id="bg-hover"
               />
               <CardHeader className="relative z-10 text-center duration-500 opacity-0 group-hover:opacity-100">
-                <CardTitle>Simple Calculator</CardTitle>
+                <CardTitle>{project.name}</CardTitle>
               </CardHeader>
               <CardContent className="relative z-10 duration-500 opacity-0 group-hover:opacity-100">
                 <div className="flex space-x-3">
@@ -90,6 +77,17 @@ const Projects: React.FC = () => {
                   ))}
                 </div>
               </CardContent>
+              <CardFooter className="relative z-10 flex items-center justify-between duration-500 opacity-0 gorup-hover:duration-0 group-hover:opacity-100">
+                <div className="text-muted-foreground">
+                  <p>{project.dateCreated}</p>
+                </div>
+                <Dialog>
+                  <DialogTrigger>
+                    <Button>Read more</Button>
+                  </DialogTrigger>
+                  <DialogContent></DialogContent>
+                </Dialog>
+              </CardFooter>
             </Card>
           ))}
         </div>
