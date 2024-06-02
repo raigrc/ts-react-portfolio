beaafff570baa4723b8e24b7c4b737b9b76b488a
raigrc <ravenjstn.grc@gmail.com>
2024-06-02 15:22:38 +0800

    fixed animation

--- src/components/pages/SVGImage.tsx
+++ src/components/pages/SVGImage.tsx
@@ -6,7 +6,7 @@ type Image = {
 
 const SVGImage: React.FC<Image> = ({ img }) => {
   return (
-    <div className="animate-fade animate-once animate-duration-1000 animate-ease-in-outabsolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center opacity-20 lg:relative lg:left-0 lg:top-0 lg:w-1/2 lg:-translate-x-0 lg:-translate-y-0 lg:opacity-100">
+    <div className="absolute flex flex-col items-center justify-center -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 lg:animate-fade opacity-20 animate-duration-700 animate-once animate-ease-in-out lg:relative lg:left-0 lg:top-0 lg:w-1/2 lg:-translate-x-0 lg:-translate-y-0 lg:opacity-100">
       <img src={img} alt={img} />
     </div>
   );
