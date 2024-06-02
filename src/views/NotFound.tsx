42ecf830b9dc2c0363b66bac67e43b532e52eb24
raigrc <ravenjstn.grc@gmail.com>
2024-06-02 15:35:10 +0800

    add typwriter

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -4,12 +4,13 @@ import NotFoundIllu from "/404/alien_taken.svg";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import SVGImage from "@/components/pages/SVGImage";
+import { useTypewriter } from "react-simple-typewriter";
 
 const NotFound: React.FC = () => {
   useDocumentTitle("Not Found!");
   return (
     <section className="h-screen">
-      <div className="mx-auto flex h-full max-w-screen-xl p-3 lg:flex-row lg:justify-between">
+      <div className="flex h-full max-w-screen-xl p-3 mx-auto lg:flex-row lg:justify-between">
         <NotFountTextInfo />
         <SVGImage img={NotFoundIllu} />
       </div>
@@ -18,22 +19,28 @@ const NotFound: React.FC = () => {
 };
 
 const NotFountTextInfo: React.FC = () => {
+  const [AlienLang] = useTypewriter({
+    words: ["☍⟟⎅⋏⏃⌿"],
+  });
+  const [AlienLang2] = useTypewriter({
+    words: ["⊬⍜⎍⍀ ⎍⋏⎅⟒⍀⌇⏁⏃⋏⎅⟟⋏☌"],
+  });
   return (
-    <div className="z-50 flex w-full flex-col items-center justify-center space-y-3 lg:w-1/2 lg:items-start">
+    <div className="z-50 flex flex-col items-center justify-center w-full space-y-3 lg:w-1/2 lg:items-start">
       <h1 className="bg-gradient-to-b from-primary to-background bg-clip-text text-9xl font-black tracking-widest text-transparent md:text-[192px] lg:text-[240px]/tight">
         404
       </h1>
-      <h3 className="text-3xl uppercase tracking-wide text-muted-foreground">
+      <h3 className="text-3xl tracking-wide uppercase text-muted-foreground">
         Page not found!
       </h3>
       <p className="py-3 text-center text-base/relaxed lg:text-left">
         Oh no! It looks like you got lost and found by the aliens.
         <br /> We couldn't find the page you are looking for.
         <br /> Please go back before they{" "}
-        <span className="font-bold text-primary">☍⟟⎅⋏⏃⌿</span> you.
+        <span className="font-bold text-primary">{AlienLang}</span> you.
         <br />
         Thank you for{" "}
-        <span className="font-bold text-primary">⊬⍜⎍⍀ ⎍⋏⎅⟒⍀⌇⏁⏃⋏⎅⟟⋏☌</span>
+        <span className="font-bold text-primary">{AlienLang2}</span>
       </p>
       <div>
         <Link to="/">
